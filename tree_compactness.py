437e34399cddf7aee118a1993d2f2dd684176b96
Lorenzo Najt <lnajt4@gmail.com>
2018-07-02 10:47:53 -0400

    enumeration

--- tree_compactness.py
+++ tree_compactness.py
@@ -11,6 +11,13 @@ from scipy.sparse import csc_matrix
 import scipy
 import random
 import numpy as np
+import copy
+from anytree import Node, NodeMixin
+
+class meta_tree(NodeMixin):
+    def __init__(self, G):
+        self.graph = G
+    
 def log_weighted_number_trees(G):
     m = nx.laplacian_matrix(G, weight = "weight")[1:,1:]
     m = csc_matrix(m)
@@ -22,43 +29,44 @@ def log_weighted_number_trees(G):
     LU_prod = np.sum(S_log_U) + np.sum(S_log_L)
     return LU_prod
 
-
-m = 3
-n = 3
-d = 2
-G = nx.grid_graph([m,n])
-H = nx.grid_graph([m,n])
-for x in G.edges():
-    a = x[0]
-    b = x[1]
-    G.edges[x]["weight"] = (np.abs(a[0] - b[0]) + np.abs(a[1] - b[1]))
-for x in H.edges():
-    a = x[0]
-    b = x[1]
-    H.edges[x]["weight"] = (d*np.abs(a[0] - b[0]) + (1/d)* np.abs(a[1] - b[1]))
-
-
-W_G = log_weighted_number_trees(G)
-W_H = log_weighted_number_trees(H)
-print("WG",W_G)
-print("WH", W_H)
-print("W_G - W_H", W_G - W_H)
-
-#W_G 98.44804291763761
-#W_H 209.95528522499345
-
-    
-ambient = nx.grid_graph( [100,100])
-
-G_nodes = [(50,50)]
-G = nx.induced_subgraph(ambient, G_nodes)
-neighbors = []
-for x in ambient.nodes():
-    if set(ambient.neighbors(x)).intersection(set(G.nodes())) != set():
-        neighbors.append(x)
-
+#
+#m = 3
+#n = 3
+#d = 2
+#G = nx.grid_graph([m,n])
+#H = nx.grid_graph([m,n])
+#for x in G.edges():
+#    a = x[0]
+#    b = x[1]
+#    G.edges[x]["weight"] = (np.abs(a[0] - b[0]) + np.abs(a[1] - b[1]))
+#for x in H.edges():
+#    a = x[0]
+#    b = x[1]
+#    H.edges[x]["weight"] = (d*np.abs(a[0] - b[0]) + (1/d)* np.abs(a[1] - b[1]))
+#
+#
+#W_G = log_weighted_number_trees(G)
+#W_H = log_weighted_number_trees(H)
+#print("WG",W_G)
+#print("WH", W_H)
+#print("W_G - W_H", W_G - W_H)
+#
+##W_G 98.44804291763761
+##W_H 209.95528522499345
+#
+#    
+#ambient = nx.grid_graph( [100,100])
+#
+#G_nodes = [(50,50)]
+#G = nx.induced_subgraph(ambient, G_nodes)
+#neighbors = []
+#for x in ambient.nodes():
+#    if set(ambient.neighbors(x)).intersection(set(G.nodes())) != set():
+#        neighbors.append(x)
+#
 
 '''
+On how to enumerate the partitions:
 One way to solve the problem is to define a tree structure on these subgraphs as follows: choose an arbitrary assignment of distinct weights to the edges of the input graph. Define the parent of a connected induced subgraph to be the graph formed by finding its minimum spanning tree, removing the leaf edge of maximum weight, and forming the induced subgraph of the remaining vertices. For a subgraph with only one vertex, define its parent to be the empty graph (which forms the root of the tree structure)
 
 Reverse search (essentially, DFS of this tree) will then find each connected induced subgraph in time polynomial per subgraph. You can find the children of any node in the tree by trying all ways of adding one vertex and checking which of them would form the heaviest leaf of the MST of the augmented subgraph; finding all the children of a single node takes polynomial time, which is all that's required to make this work.
@@ -72,7 +80,9 @@ for e in G_edges:
     
 def parent(H):
     if len(H.nodes()) == 1:
-        return []
+        return set([])
+    if len(H.nodes()) == 2:
+        return set(H.nodes())
     M = nx.minimum_spanning_tree(H)
     M_edges = M.edges()
     leaf_edges = [f for f in M_edges if ( (M.degree(f[0]) == 1) or (M.degree(f[1]) == 1))]
@@ -84,12 +94,12 @@ def parent(H):
             components = list(nx.connected_components(M))
             if len(components[1]) == 1:
 
-                print(components[1])
-               # return set(components[0])
+               # print(components[0])
+                return set(components[0])
             else:
 
-                print(components[0])
-               # return set(components[1])
+                #print(components[1])
+                return set(components[1])
         
 def neighbors(H_nodes, G):
     #Finds all neighbor nodes of H in G
@@ -110,16 +120,33 @@ def children(H_nodes,G):
     N = neighbors(H_nodes,G)
 
     #input H as a list of nodes
-    admissable_children = []
+    admissable_additions = []
     for n in N:
         H_nodes.add(n)
         candidate_child = nx.induced_subgraph(G, H_nodes)
         H_nodes.remove(n)
         P = parent(candidate_child)
-        if P == H_nodes:
-            admissable_children.append(n)
+        if type(P) == set:
+            if P.intersection(H_nodes) != set():
+                admissable_additions.append(n)
+        else:
+            if P == H_nodes:
+                admissable_additions.append(n)
+    admissable_children = []
+    for n in admissable_additions:
+        H_nodes.add(n)
+        admissable_children.append(copy.deepcopy(H_nodes))
+        H_nodes.remove(n)
     return admissable_children
 
 def build_tree(G):
-    root = nx.Graph()
+    G = nx.Graph()
+    root = meta_tree(G)
+    
+    
+def prune(T):
+    #Remove isomorphism duplicates from among the subgraphs
+    
+def extend(T):
+    
     
